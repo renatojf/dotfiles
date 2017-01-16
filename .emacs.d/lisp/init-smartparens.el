9655e136bc3834dfaff3ccf0008ade6d891a5736
Renato Correa <renato.correa@fyber.com>
2017-01-16 16:27:13 +0100

    SmartParens kill region fix.

--- .emacs.d/lisp/init-smartparens.el
+++ .emacs.d/lisp/init-smartparens.el
@@ -7,4 +7,11 @@
 
 (smartparens-global-mode t)
 
+(defun my-kill-region-or-line (&optional arg)
+  "Kill active region or current line."
+  (interactive "p")
+  (if (use-region-p)
+      (sp-kill-region (region-beginning) (region-end)) ;; strict-mode version of kill-region
+    (sp-kill-whole-line))) ;; strict-mode version of kill-whole-line
+
 (provide 'init-smartparens)
